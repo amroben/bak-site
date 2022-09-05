efd532f78e1548d81c17645f7e2f5b6248d70fdb
amroben <111748082+amroben@users.noreply.github.com>
2022-09-05 22:43:39 +0100

    Add files via upload

---
+++ phone store/java.js
@@ -0,0 +1,13 @@
+let img=document.getElementById('img');
+let bac=document.getElementById('bac');
+
+function phon(phone){
+img.src= phone;
+
+}
+
+function color(col){
+    bac.style.background =col;
+    
+    }
+    
